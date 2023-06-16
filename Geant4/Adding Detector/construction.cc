a4b41028ff634662112af732ea8b7d423e4760d5
Anantha Padmanabhan M Nair <apmnair18@gmail.com>
2023-06-16 20:51:48 +0530

    Mmm

--- Geant4/Adding Detector/construction.cc
+++ Geant4/Adding Detector/construction.cc
@@ -1,5 +1,4 @@
 #include "construction.hh"
-#include "G4Tubs.hh"
 
 MyDetectorConstruction::MyDetectorConstruction()
 {}
@@ -9,10 +8,11 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct()
 {
     G4NistManager *nist = G4NistManager::Instance();
     G4Material *worldMat = nist->FindOrBuildMaterial("G4_WATER");
-    G4Box *solidWorld = new G4Box("solidWorld",0.5*m,0.05*m,0.1*m);
-    G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, worldMat,"logicWorld");
-    G4VPhysicalVolume *physWorld = new G4PVPlacement(0,G4ThreeVector(0,0,0),logicWorld, "physWorld", 0, false,0,true);
-    return physWorld;
+    // G4Box *sbox = new G4Box("sbox",0.5*m,0.05*m,0.1*m);
+    G4Tubs* sbox = new G4Tubs("sbox",1*m,5*m,25*m,0,360*deg);
+    G4LogicalVolume *lbox = new G4LogicalVolume(sbox, worldMat,"lbox");
+    G4VPhysicalVolume *pbox = new G4PVPlacement(0,G4ThreeVector(0,0,0),lbox, "pbox", 0, false,0,true);
+    return pbox;
 }
 
 
