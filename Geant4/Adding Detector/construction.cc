fcfe7c7812f2c76e354676f501c741c2efc8b640
Anantha Padmanabhan <apmnair18@gmail.com>
2023-06-19 17:24:26 +0530

    Edit HC

--- Geant4/Adding Detector/construction.cc
+++ Geant4/Adding Detector/construction.cc
@@ -6,8 +6,18 @@ scintillation1::~scintillation1()
 {}
 G4VPhysicalVolume *scintillation1::Construct()
 {
-    G4double zp[] =  {0,5.0*mm};
-    G4double ir[]= {0,5.0*mm};
+    G4double zp[2] =  {0,5};
+    G4double ir[2]= {0,5};
+    G4double detectorRadius = 10.0;   // Radius of the detector
+    G4double detectorHeight = 20.0;  // Height of the detector
+    double ang=360*deg;
+    int ang1= 0;
+
+    // Number of sides for the regular hexagon
+    G4int numSides = 6;
+
+
+
 
     // Define materials
     G4NistManager* nistManager = G4NistManager::Instance();
@@ -19,7 +29,7 @@ G4VPhysicalVolume *scintillation1::Construct()
     G4LogicalVolume *logicworld = new G4LogicalVolume(solidworld, air,"logicalworld");
     G4VPhysicalVolume *physicalworld = new G4PVPlacement(nullptr,G4ThreeVector(0,0,0),logicworld, "physicalworld", nullptr, false,0,true);
 
-    //defining 1st detector
+    //defining 1st Detector
     G4Box* solidDetector1 = new G4Box("Detector1", 1*m,0.05*m,0.1*m);
     G4LogicalVolume* logicalDetector1 = new G4LogicalVolume(solidDetector1, detectorMaterial, "Detector1");
     new G4PVPlacement(nullptr, G4ThreeVector(0, -1*m, 0), logicalDetector1, "Detector1", logicworld, false, 0,true);
@@ -30,16 +40,10 @@ G4VPhysicalVolume *scintillation1::Construct()
     new G4PVPlacement(nullptr, G4ThreeVector(0, 1*m,0), logicalDetector2, "Detector2", logicworld, false, 0,true);
 
     //defining Honeycomb detector
-    // G4Tubs* hcdetector = new G4Tubs("honeycomb",0,5*mm,2.5*mm,0,2*M_PI);
-    G4Polyhedra *hcdetector = new G4Polyhedra("DetectorSolid",0,360 * deg,6,2,zp,ir,ir);
+    G4Polyhedra *hcdetector = new G4Polyhedra("DetectorSolid",0,360*deg,numSides,1,zp[2],ir[2],ir[2]);
     G4LogicalVolume* hclogic = new G4LogicalVolume(hcdetector,detectorMaterial,"hclogic");
-    new G4PVPlacement(nullptr, G4ThreeVector(0,0,0), logicalDetector1, "HCdetector", logicworld, false, 0,true);
+    new G4PVPlacement(nullptr, G4ThreeVector(0,0,0), hcdetector, "HCdetector", logicworld, false, 0,true);
 
-
-
-
-
-    // return new G4PVPlacement(nullptr, G4ThreeVector(), logicworld, "World", nullptr, false, 0,true);
     return physicalworld;
 
 }
