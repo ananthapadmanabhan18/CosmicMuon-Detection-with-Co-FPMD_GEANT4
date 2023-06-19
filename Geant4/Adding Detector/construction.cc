da079182a93769792b274575c196b978cf856b94
Anantha Padmanabhan <apmnair18@gmail.com>
2023-06-19 14:20:12 +0530

    placing Detector

--- Geant4/Adding Detector/construction.cc
+++ Geant4/Adding Detector/construction.cc
@@ -8,7 +8,7 @@ G4VPhysicalVolume *scintillation1::Construct()
 {
     // Define materials
     G4NistManager* nistManager = G4NistManager::Instance();
-    G4Material* air = nistManager->FindOrBuildMaterial("G4_AIR");
+    G4Material* air = nistManager->FindOrBuildMaterial("G4_Al");
     G4Material* detectorMaterial = nistManager->FindOrBuildMaterial("G4_Ge");
 
     //Defining World Volume
@@ -19,12 +19,12 @@ G4VPhysicalVolume *scintillation1::Construct()
     //defining 1st detector
     G4Box* solidDetector1 = new G4Box("Detector1", 1*m,0.05*m,0.1*m);
     G4LogicalVolume* logicalDetector1 = new G4LogicalVolume(solidDetector1, detectorMaterial, "Detector1");
-    new G4PVPlacement(nullptr, G4ThreeVector(0, 0, 0), logicalDetector1, "Detector1", logicworld, false, 0,true);
+    new G4PVPlacement(nullptr, G4ThreeVector(0, -1*m, 0), logicalDetector1, "Detector1", logicworld, false, 0,true);
 
     //defining 2nd Detector
     G4Box* solidDetector2 = new G4Box("Detector2", 1*m,0.05*m,0.1*m);
     G4LogicalVolume* logicalDetector2 = new G4LogicalVolume(solidDetector2, detectorMaterial, "Detector2");
-    new G4PVPlacement(nullptr, G4ThreeVector(0, 2*m,0), logicalDetector2, "Detector2", logicworld, false, 0,true);
+    new G4PVPlacement(nullptr, G4ThreeVector(0, 1*m,0), logicalDetector2, "Detector2", logicworld, false, 0,true);
 
 
     logicworld->SetVisAttributes(G4VisAttributes::GetInvisible); // Make the world volume invisible
