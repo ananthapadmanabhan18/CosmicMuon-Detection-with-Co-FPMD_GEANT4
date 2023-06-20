f75cf92f888dcb172b68dcf519fe6e4a7566ac0e
Anantha Padmanabhan <apmnair18@gmail.com>
2023-06-20 16:12:21 +0530

    Trying to add Gas Mixture

--- Geant4/Adding Detector/construction.cc
+++ Geant4/Adding Detector/construction.cc
@@ -7,6 +7,8 @@ scintillation1::~scintillation1()
 {}
 G4VPhysicalVolume *scintillation1::Construct()
 {
+
+
     // Set up the dimensions of the honey comb detector
     G4double phiStart = - 0 * M_PI;
     G4double phiTotal =  2* M_PI; // Change this value as needed
@@ -15,25 +17,42 @@ G4VPhysicalVolume *scintillation1::Construct()
     G4double rInner[]= {4.8*mm, 4.6*mm};
     G4double rOuter[] = {5*mm, 5*mm};
     G4double zPlanes[]={-2.5*mm,2.5*mm};
+    G4double gInner[]= {0.01*mm, 0.01*mm};
+    G4double gOuter[] = {4.8*mm, 4.8*mm};
+
 
 
     // Define materials
     G4NistManager* nistManager = G4NistManager::Instance();
-    G4Material* air = nistManager->FindOrBuildMaterial("G4_Al");
-    G4Material* detectorMaterial = nistManager->FindOrBuildMaterial("G4_Ge");
+    G4Material* air = nistManager->FindOrBuildMaterial("G4_AIR");
+    G4Material* ge = nistManager->FindOrBuildMaterial("G4_Ge");
     G4Material* gold = nistManager->FindMaterial("G4_Au");
     G4Material* copper = nistManager->FindOrBuildMaterial("G4_Cu");
+    //Argon
+
+    //Carbon Monoxide
+
+
+
+
 
     //Defining World Volume
     G4Box *solidworld = new G4Box("world",2.5*m,2.5*m,2.5*m);
     G4LogicalVolume *logicworld = new G4LogicalVolume(solidworld, air,"logicalworld");
     G4VPhysicalVolume *physicalworld = new G4PVPlacement(nullptr,G4ThreeVector(0,0,0),logicworld, "physicalworld", nullptr, false,0,true);
 
+
+
+
+
     //defining 2 Scintillation Detectors Detector
     G4Box* solidDetector = new G4Box("Detector1", 16*(30/sqrt(3))*mm,16*(10)*mm,0.05*m);
-    G4LogicalVolume* logicalDetector = new G4LogicalVolume(solidDetector, detectorMaterial, "Detector1");
+    G4LogicalVolume* logicalDetector = new G4LogicalVolume(solidDetector, ge, "Detector1");
     new G4PVPlacement(nullptr, G4ThreeVector(0, -0*m, -1*m), logicalDetector, "Detector1", logicworld, false, 0,true);
     new G4PVPlacement(nullptr, G4ThreeVector(0, 0*m,1*m), logicalDetector, "Detector2", logicworld, false, 0,true);
+
+
+
 
 
 
@@ -49,6 +68,9 @@ G4VPhysicalVolume *scintillation1::Construct()
     }
 
 
+
+
+
     //Defining the gold wire 
     G4Tubs *goldwire = new G4Tubs("goldwire",0,0.01*mm,5*mm,0,2*M_PI);
     G4LogicalVolume* goldlogic = new G4LogicalVolume(goldwire,gold,"hclogic");
@@ -58,6 +80,25 @@ G4VPhysicalVolume *scintillation1::Construct()
             new G4PVPlacement(nullptr, G4ThreeVector((i*(30/sqrt(3))-(15/sqrt(3)))*mm,((j*10)-(5))*mm,0), goldlogic, "goldwire", logicworld, false, 0,true);
         }
     }
+
+
+
+
+
+
+
+
+    // Filling the detector with argon/CO mixture
+    // G4Polyhedra *gassfill = new G4Polyhedra("gas", phiStart,phiTotal,numSide,numZplanes,zPlanes,gInner, gOuter);
+    // G4LogicalVolume* gaslogic = new G4LogicalVolume(gassfill,gasMixture,"gaslogic");
+
+    // for (int i = -15;i<=16;i++){
+    //     for (int j = -15;j<=16;j++){
+    //         new G4PVPlacement(nullptr, G4ThreeVector((i*(30/sqrt(3)))*mm,j*10*mm,0), gaslogic, "gasmix", logicworld, false, 0,true);
+    //         new G4PVPlacement(nullptr, G4ThreeVector((i*(30/sqrt(3))-(15/sqrt(3)))*mm,((j*10)-(5))*mm,0), gaslogic, "gasmix", logicworld, false, 0,true);
+    //     }
+    // }
+
 
     return physicalworld;
 }
