f49d03fb977245906f88bd8514b60a044f699ce0
Anantha Padmanabhan M Nair <apmnair18@gmail.com>
2023-07-10 01:21:40 +0530

    Plot HIstogram

--- ROOT/Data_from_geant4/histogram.py
+++ ROOT/Data_from_geant4/histogram.py
@@ -1,35 +1,20 @@
 import ROOT
+file = open("ROOT\Data_from_geant4\output.txt", "r")
 
-# Open the .txt file
-file = open("data.txt", "r")
-
-# Read the data from the file
 data = []
 for line in file:
     value = float(line.strip())
     data.append(value)
 
-# Close the file
 file.close()
 
-# Create a ROOT canvas
-canvas = ROOT.TCanvas("canvas", "Histogram", 800, 600)
+canvas = ROOT.TCanvas("canvas", "Histogram", 1600, 900)
+histogram = ROOT.TH1F("histogram", "Histogram Title", 200, 0, 0.01)
 
-# Create a histogram
-histogram = ROOT.TH1F("histogram", "Histogram Title", 100, 0, 100)
-
-# Fill the histogram with the data
 for value in data:
     histogram.Fill(value)
 
-# Set the histogram style
-histogram.SetFillColor(ROOT.kBlue)
-
-# Draw the histogram
+# histogram.SetFillColor(ROOT.kBlue)
 histogram.Draw()
-
-# Update the canvas
 canvas.Update()
-
-# Run the event loop
 ROOT.gApplication.Run()
