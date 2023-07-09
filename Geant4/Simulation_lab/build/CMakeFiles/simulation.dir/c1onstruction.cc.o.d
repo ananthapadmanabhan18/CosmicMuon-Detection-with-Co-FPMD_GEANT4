7a366b42bb8fd31547dea3784051c816e1cf22ed
Anantha Padmanabhan <apmnair18@gmail.com>
2023-07-09 18:50:54 +0530

    Edit

--- Geant4/Simulation_lab/build/CMakeFiles/simulation.dir/c1onstruction.cc.o.d
+++ Geant4/Simulation_lab/build/CMakeFiles/simulation.dir/c1onstruction.cc.o.d
@@ -1,3 +1,3 @@
 CMakeFiles/simulation.dir/c1onstruction.cc.o: \
- /home/apmnair18/Documents/GitHub/Geant4_ROOT/Geant4/Simulation_lab/c1onstruction.cc \
+ /home/apmnair18/Github_Repos/Geant4_ROOT/Geant4/Simulation_lab/c1onstruction.cc \
  /usr/include/stdc-predef.h
