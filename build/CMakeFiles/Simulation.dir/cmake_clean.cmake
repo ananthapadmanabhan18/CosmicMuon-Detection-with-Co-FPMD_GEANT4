17b297d880841acb0ec4ce944770f30b2840698e
Anantha Padmanabhan M Nair <apmnair18@gmail.com>
2023-07-14 21:03:48 +0530

    E

--- build/CMakeFiles/Simulation.dir/cmake_clean.cmake
+++ build/CMakeFiles/Simulation.dir/cmake_clean.cmake
@@ -1,8 +1,21 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Simulation"
+  "CMakeFiles/simulation.dir/action.cc.o"
+  "CMakeFiles/simulation.dir/action.cc.o.d"
+  "CMakeFiles/simulation.dir/c1onstruction.cc.o"
+  "CMakeFiles/simulation.dir/c1onstruction.cc.o.d"
+  "CMakeFiles/simulation.dir/construction.cc.o"
+  "CMakeFiles/simulation.dir/construction.cc.o.d"
+  "CMakeFiles/simulation.dir/generator.cc.o"
+  "CMakeFiles/simulation.dir/generator.cc.o.d"
+  "CMakeFiles/simulation.dir/physics.cc.o"
+  "CMakeFiles/simulation.dir/physics.cc.o.d"
+  "CMakeFiles/simulation.dir/simulation.cc.o"
+  "CMakeFiles/simulation.dir/simulation.cc.o.d"
+  "simulation"
+  "simulation.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+foreach(lang CXX)
+  include(CMakeFiles/simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
