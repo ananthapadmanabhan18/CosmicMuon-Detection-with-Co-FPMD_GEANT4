22e65e6d4d432497b804f87fb580964291fb0fb2
Anantha Padmanabhan M Nair <apmnair18@gmail.com>
2023-07-14 20:58:23 +0530

    edit

--- ROOT/Data_from_geant4/histogram.py
+++ ROOT/Data_from_geant4/histogram.py
@@ -7,8 +7,13 @@ histogram.SetYTitle("Frequency")
 
 file = open("ROOT/Data_from_geant4/output.txt", "r")
 
+
+i=0
+count=0
 for line in file:
     value = float(line.strip())
+    i=i+value
+    count+=1
     histogram.Fill(value)
 file.close()
 
@@ -23,7 +28,7 @@ print("Total Number of particles = ", totalno)
 histogram.Scale(1.0 / totalno)
 area = histogram.Integral("width")
 print("Total Energy of 1 Muon = ", area*pow(10,6), "eV")
-
+print("Avg=", (i/count)*1000000)
 
 
 histogram.Draw()
