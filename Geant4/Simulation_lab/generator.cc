90305d29733a483bbd367a846f31061f76c2ed6a
Anantha Padmanabhan M Nair <apmnair18@gmail.com>
2023-07-16 00:22:00 +0530

    Added vis.mac

--- Geant4/Simulation_lab/generator.cc
+++ Geant4/Simulation_lab/generator.cc
@@ -6,6 +6,13 @@
 PrimaryGenerator::PrimaryGenerator()
 {
     fParticleGun=new G4ParticleGun(1);
+    G4ParticleTable *ptable = G4ParticleTable::GetParticleTable();
+    G4ThreeVector pos(0.0050*m,0.0025*m,2.5*m);
+    G4ThreeVector mom(0,0,-1);
+    fParticleGun->SetParticlePosition(pos);
+    fParticleGun->SetParticleMomentumDirection(mom);
+    fParticleGun->SetParticleMomentum(1*GeV);
+    fParticleGun->SetParticleDefinition(G4MuonMinus::Definition());    
 }
 PrimaryGenerator::~PrimaryGenerator()
 {
@@ -14,12 +21,5 @@ PrimaryGenerator::~PrimaryGenerator()
 
 void PrimaryGenerator::GeneratePrimaries(G4Event*anEvent)
 {
-    G4ParticleTable *ptable = G4ParticleTable::GetParticleTable();
-    G4ThreeVector pos(0.0050*m,0.0025*m,2.5*m);
-    G4ThreeVector mom(0,0,-1);
-    fParticleGun->SetParticlePosition(pos);
-    fParticleGun->SetParticleMomentumDirection(mom);
-    fParticleGun->SetParticleMomentum(1*GeV);
-    fParticleGun->SetParticleDefinition(G4MuonMinus::Definition());
     fParticleGun->GeneratePrimaryVertex(anEvent);
 }
