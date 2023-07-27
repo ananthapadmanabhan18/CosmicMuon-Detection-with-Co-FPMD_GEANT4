4fd5f11a6ff984e4bac1da57e75a1f0ccee1c41e
Anantha Padmanabhan M Nair <apmnair18@gmail.com>
2023-07-28 00:41:05 +0530

    Analysis

--- ROOT/Final_Analysis/Single_Energy/Single_Histogram.py
+++ ROOT/Final_Analysis/Single_Energy/Single_Histogram.py
@@ -1,6 +1,6 @@
 import ROOT
 canvas = ROOT.TCanvas("canvas", "Histogram", 1600, 900)
-histogram = ROOT.TH1F("histogram", "Plot of Frequency  vs Energy Deposit  (GeV)", 750, 0, 0.01)
+histogram = ROOT.TH1F("histogram", "Plot of Frequency  vs Energy Deposit  (GeV)", 100, 0, 0.005)
 
 histogram.SetXTitle("Incident Energy of the Muons on the Gas Mixture (GeV)")
 histogram.SetYTitle("Frequency (No of Particles in the Energy Range)")
@@ -8,7 +8,7 @@ histogram.SetLineColor(ROOT.kRed)
 
 
 
-file = open("Final_cosmic_data.txt", "r")
+file = open("datas/Final_cosmic_data.txt", "r")
 
 
 i=0
@@ -25,10 +25,10 @@ file.close()
 # histogram.Scale(1/integral)
 
 
-landau_func = ROOT.TF1("landau_func", "landau", 0.0004,0.004)
-landau_func.SetParameters(0.0001,0.0007)
-histogram.Fit(landau_func, "R")
-fit_result = histogram.GetFunction("landau_func")
+# landau_func = ROOT.TF1("landau_func", "landau", 0.0004,0.004)
+# landau_func.SetParameters(0.0001,0.0007)
+# histogram.Fit(landau_func, "R")
+# fit_result = histogram.GetFunction("landau_func")
 
 # mpv = fit_result.GetParameter(1)
 # sigma = fit_result.GetParameter(2)
