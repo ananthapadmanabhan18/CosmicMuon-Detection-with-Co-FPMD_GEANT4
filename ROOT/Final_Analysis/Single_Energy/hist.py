48d63d64702cff519d617c8c904131b40655553e
Anantha Padmanabhan M Nair <apmnair18@gmail.com>
2023-09-06 21:09:23 +0530

    Update hist.py

--- ROOT/Final_Analysis/Single_Energy/hist.py
+++ ROOT/Final_Analysis/Single_Energy/hist.py
@@ -1,7 +1,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import PchipInterpolator
 
 
 file = open("Single_Energy/datas/Final_cosmic_data.txt", "r")
@@ -24,37 +23,10 @@ ylist = [2.23347e-03,6.22615e-04,5.31900e-04,5.30453e-04,5.379379e-04,
     7.64237e-04,8.11232e-04,8.35784e-04,8.43543e-04,8.47351e-04]  
 
 
-pointlist=[(ylist[i],xlist[i]) for i in range(len(xlist))]
-print(pointlist)
-def sortlist(pointlist):
-    return sorted(pointlist)
-
-pointlist1=sortlist(pointlist)
-
-print(pointlist1)
 
 
-xlist1=[pointlist1[i][0] for i in range(len(pointlist1))]
-ylist1=[pointlist1[i][1] for i in range(len(pointlist1))]
-
-
-# Perform piecewise cubic Hermite interpolation (PCHIP)
-interp = PchipInterpolator(xlist1, ylist1)
-
-# Calculate the interpolated y values
-y_interp = interp(ylist2)
-
-# Print the interpolated y values
-for x, y in zip(ylist2, y_interp):
-    print(f"For x = {x}, interpolated y = {y}")
-
-
-print(len(xlist1))
-print(len(y_interp))
-
-
-
-plt.plot(xlist1,y_interp,'r',label='MCNP6') 
+plt.plot(xlist,ylist,'r',label='MCNP6') 
+plt.scatter(xlist,ylist) 
 plt.show()
 
 
