c8b522b02ee510a671a91308a6699616752bf5e1
Anantha Padmanabhan M Nair <apm.nair@niser.ac.in>
2023-11-13 23:06:08 +0530

    bleh

--- Geant4/Working Folder/build/cmake_install.cmake
+++ Geant4/Working Folder/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/apmnair18/Documents/GitHub/VECC_Internship_2023/Geant4/Working Folder
+# Install script for directory: C:/Users/apmna/Documents/GitHub/VECC_Internship_2023/Geant4/Working Folder
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/apmna/Documents/GitHub/VECC_Internship_2023/Geant4/Working Folder/build/app")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -27,19 +27,21 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/apmna/Documents/GitHub/VECC_Internship_2023/Geant4/Working Folder/build/Debug/simulation.exe")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/apmna/Documents/GitHub/VECC_Internship_2023/Geant4/Working Folder/build/Release/simulation.exe")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/apmna/Documents/GitHub/VECC_Internship_2023/Geant4/Working Folder/build/MinSizeRel/simulation.exe")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/apmna/Documents/GitHub/VECC_Internship_2023/Geant4/Working Folder/build/RelWithDebInfo/simulation.exe")
+  endif()
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +52,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/apmnair18/Documents/GitHub/VECC_Internship_2023/Geant4/Working Folder/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/apmna/Documents/GitHub/VECC_Internship_2023/Geant4/Working Folder/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
