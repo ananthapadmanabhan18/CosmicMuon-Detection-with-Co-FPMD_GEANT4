c0f9bed6fa9c6eff83c72c80e7b319f7b91e404b
Anantha Padmanabhan M Nair <apm.nair@niser.ac.in>
2024-05-16 00:50:36 +0530

    Multithreading

--- src/generator.cc
+++ src/generator.cc
@@ -6,7 +6,7 @@
 
 primarygenerator::primarygenerator(){
 
-
+    fparticlegun = new G4ParticleGun(1);
 
     ecoMug = new EcoMug();
     ecoMug->SetUseSky();
@@ -14,7 +14,14 @@ primarygenerator::primarygenerator(){
     ecoMug->SetSkyCenterPosition({{0*m,0*m,5*m}});
 
 
-    fparticlegun = new G4ParticleGun(1);
+    fparticlegun->SetParticleDefinition(G4MuonMinus::Definition());
+    G4ThreeVector pos(0,0,5*m);
+    fparticlegun->SetParticlePosition(pos);
+    G4double mom = 1*GeV;
+    fparticlegun->SetParticleMomentum(mom);
+    fparticlegun->SetParticleMomentumDirection(G4ThreeVector(0,0,-1));
+
+
      
 }
 
@@ -26,34 +33,34 @@ primarygenerator::~primarygenerator(){
 void primarygenerator::GeneratePrimaries(G4Event *anEvent)
 {
 
-    ecoMug->Generate();
+    // ecoMug->Generate();
 
-    //Setting the Particle according to charge
-    int particle_charge = ecoMug->GetCharge();
-    if(particle_charge == 1){
-        fparticlegun->SetParticleDefinition(G4MuonPlus::Definition());
-    }
-    else if(particle_charge == -1){
-        fparticlegun->SetParticleDefinition(G4MuonMinus::Definition());
-    }
-    else{
-        G4cout << "Error: Particle charge not recognized" << G4endl;
-    }
-
-
-
-    // Setting the Position
-    std::array<G4double, 3> position = ecoMug->GetGenerationPosition();
-    G4ThreeVector pos(position[0]*mm, position[2]*mm, position[1]*mm);
-    fparticlegun->SetParticlePosition(pos);
+    // //Setting the Particle according to charge
+    // int particle_charge = ecoMug->GetCharge();
+    // if(particle_charge == 1){
+    //     fparticlegun->SetParticleDefinition(G4MuonPlus::Definition());
+    // }
+    // else if(particle_charge == -1){
+    //     fparticlegun->SetParticleDefinition(G4MuonMinus::Definition());
+    // }
+    // else{
+    //     G4cout << "Error: Particle charge not recognized" << G4endl;
+    // }
 
 
-    //Setting the Momentum
-    G4double momentum = ecoMug->GetGenerationMomentum();
-    G4double theta = ecoMug->GetGenerationTheta();
-    G4double phi = ecoMug->GetGenerationPhi();
-    G4ThreeVector mom(momentum*sin(theta)*cos(phi)*GeV, momentum*cos(theta)*GeV, momentum*sin(theta)*sin(phi)*GeV);
-    fparticlegun->SetParticleMomentum(mom);
+
+    // // Setting the Position
+    // std::array<G4double, 3> position = ecoMug->GetGenerationPosition();
+    // G4ThreeVector pos(position[0]*mm, position[2]*mm, position[1]*mm);
+    // fparticlegun->SetParticlePosition(pos);
+
+
+    // //Setting the Momentum
+    // G4double momentum = ecoMug->GetGenerationMomentum();
+    // G4double theta = ecoMug->GetGenerationTheta();
+    // G4double phi = ecoMug->GetGenerationPhi();
+    // G4ThreeVector mom(momentum*sin(theta)*cos(phi)*GeV, momentum*cos(theta)*GeV, momentum*sin(theta)*sin(phi)*GeV);
+    // fparticlegun->SetParticleMomentum(mom);
 
     fparticlegun->GeneratePrimaryVertex(anEvent);
 }
--- src/stepping.cc
+++ src/stepping.cc
@@ -15,8 +15,8 @@ void steppingaction::UserSteppingAction(const G4Step *step)
     G4ParticleDefinition* def =  step->GetTrack()->GetDefinition();
     if(def == G4MuonPlus::Definition() || def == G4MuonMinus::Definition()){
         fEventAction->AddEdep(edep);
-        G4cout << " Muon Detected" << G4endl;
-        G4cout << "Energy deposited: " << edep/MeV << " MeV" << G4endl;
+        // G4cout << " Muon Detected" << G4endl;
+        // G4cout << "Energy deposited: " << edep << " MeV" << G4endl;
 
     }
     
