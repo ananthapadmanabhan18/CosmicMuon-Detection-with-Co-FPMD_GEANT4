ba5cf9bc20659cf3989cb70750a393ebf0bb383a
Anantha Padmanabhan M Nair <apmnair18@gmail.com>
2024-07-07 22:44:56 +0530

    Renaming

---
+++ Analysis/Calibration.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sstream>
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TMath.h"
+
+
+void Calibration()
+{
+    char* filelist[20] = {
+        "..//Outputs//Calibration//1GeV.root",
+        "..//Outputs//Calibration//1GeV.root",
+        "..//Outputs//Calibration//1GeV.root",
+        "..//Outputs//Calibration//1GeV.root",
+        "..//Outputs//Calibration//1GeV.root",
+        "..//Outputs//Calibration//1GeV.root",
+        "..//Outputs//Calibration//1GeV.root",
+        "..//Outputs//Calibration//1GeV.root",
+        "..//Outputs//Calibration//1GeV.root",
+        "..//Outputs//Calibration//1GeV.root",
+        "..//Outputs//Calibration//1GeV.root",
+        "..//Outputs//Calibration//1MeV.root",
+        "..//Outputs//Calibration//1MeV.root",
+        "..//Outputs//Calibration//1MeV.root",
+        "..//Outputs//Calibration//1MeV.root",
+        "..//Outputs//Calibration//1MeV.root",
+        "..//Outputs//Calibration//1MeV.root",
+        "..//Outputs//Calibration//1MeV.root",
+        "..//Outputs//Calibration//1MeV.root",
+        "..//Outputs//Calibration//1MeV.root"
+    }; 
+}
